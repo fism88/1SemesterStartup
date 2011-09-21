727d080f7e0c531e5d287a37c87e3c6e7c429fe7
Faraaz Ismail <fism88@gmail.com>
2011-09-21 12:27:12 -0500

    clean up

---
+++ Planda/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Planda::Application.config.secret_token = '695b67df920ea335a9a061fcf790ac0f3e073730a6e49e842702d0cc10fb86a8a24659f456b9c889b474b13a2ae8e038c52df8a21537315f40cc931a7581b649'
